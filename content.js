4922a22035fe3e467addf6d9ec0a0f8120ed45d3
unknown <alngregory@gmail.com>
2020-12-07 22:55:18 -0300

    Alterando a velocidade de 1x para 2x e vice-versa. Incluído também os comentários no código.

--- content.js
+++ content.js
@@ -1,22 +1,42 @@
 const intervalo = setInterval(()=>{
-    const header = document.querySelector("._2O84H");
+
+    //Constante para pegar a tag header, onde vou adicionar o botão de 2x
+    const header = document.querySelector("header");
+
+    //Se encontrar o header eu entro aqui
     if (header) {
         console.log(header)
+
+        //E paro o intervalo da busca pelo elemento
         clearInterval(intervalo)
 
+        //Crio um elemento do botão
         const botaoVelocidade = document.createElement("button")
+        botaoVelocidade.title = "Velocidade Áudio"
         botaoVelocidade.innerHTML = "2x"
         botaoVelocidade.classList.add("botaoVelocidade")
 
+        //Faço que o click dele modifique a propriedade playbackRate para 2 (2x)
         botaoVelocidade.addEventListener("click", () => {
             const audios = document.querySelectorAll("audio");
             console.log(audios)
+
+            //Para cada áudio eu verifico qual está configurado no momento
             audios.forEach((audio) => {
                 console.log(audio)
-                audio.playbackRate = 2;
+                if (audio.playbackRate == 1) {
+                    //Chegou na velocidade 1, mudo o texto para 1x e a velocidade para 2x
+                    botaoVelocidade.innerHTML = "1x";
+                    audio.playbackRate = 2;
+                } else {
+                    //Chegou na velocidade 2, mudo o texto para 2x e a velocidade para 1x
+                    botaoVelocidade.innerHTML = "2x";
+                    audio.playbackRate = 1;
+                }
             })
         })
 
+        //Adiciono o botão no header
         header.appendChild(botaoVelocidade)
     }
 })
