d26edc409816e177f51259aa4f05dcf11d995735
unknown <alngregory@gmail.com>
2020-12-07 21:55:01 -0300

    Incluindo botão 2x no header e aumentando a velocidade dos áudios nas conversas para 2x.

--- content.js
+++ content.js
@@ -1,1 +1,22 @@
-console.log("Extensão rodando pela primeira vez!");
+const intervalo = setInterval(()=>{
+    const header = document.querySelector("._2O84H");
+    if (header) {
+        console.log(header)
+        clearInterval(intervalo)
+
+        const botaoVelocidade = document.createElement("button")
+        botaoVelocidade.innerHTML = "2x"
+        botaoVelocidade.classList.add("botaoVelocidade")
+
+        botaoVelocidade.addEventListener("click", () => {
+            const audios = document.querySelectorAll("audio");
+            console.log(audios)
+            audios.forEach((audio) => {
+                console.log(audio)
+                audio.playbackRate = 2;
+            })
+        })
+
+        header.appendChild(botaoVelocidade)
+    }
+})
